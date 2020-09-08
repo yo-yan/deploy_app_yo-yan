2a06fdcac2574dc8596e76f4108819f5e0140a8d
yo-yan <yooyan0512@gmail.com>
2020-09-08 14:48:09 +0900

    レベル-3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <button onClick={() => { alert('Helloworld') }}>Click me</button>
-    </div>
+      <ButtonGroup size="sm" className="mb-2">
+        <Button onClick={() => setCount(count + 1)}> いいね！</Button>
+        <Button onClick={() => setCount(count - 1)}>良くないね。</Button>
+        <Button onClick={() => setCount(0)}>reset</Button>
+      </ButtonGroup>
+      {count}
+    </div >
   )
 }
 
